0416cb706772ac436900474421f192acfd1be3e3
nickthathasnotbeentakenyet <44579671+nickthathasnotbeentakenyet@users.noreply.github.com>
2020-06-15 22:24:57 +0300

    Update userManager.sh

--- userManager.sh
+++ userManager.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 #
 # Author: warning
 # userManager v1.0
@@ -6,9 +6,9 @@
 red='\e[31m'   # red
 green='\e[32m' # green
 cyan='\e[96m'  # cyan
-blink='\e[5m'  # blink
 def='\e[0m'    # default
 bold='\e[1m'   # bold
+yel='\e[93m'   # yellow	
 
 # Making sure Captain is on board
 if [[ $(id -u) -ne 0 ]]
@@ -16,71 +16,59 @@ if [[ $(id -u) -ne 0 ]]
 		echo -e "${red}Please run as root${def}"
 		exit 
 fi
-# Displaying Welcome message
-if [[ "$1" = "" ]]
-	then
+
+case $1 in
+	# Displaying Welcome message
+	"")
+		figlet UserManager | lolcat
 		echo -e "${cyan}\nWelcome to userManager utility\n\nAvailable keys are: \n${red}-a ${cyan}[add a user]\n${red}-b ${cyan}[delete a user]\n${red}-h ${cyan}[print help mesage]\nexample: myuseradd.sh -h\n${def}"
-fi
-# Got lost? Need help? 
-if [[ "$1" = -h ]] || [[ "$1" = --help ]]
-	then 
+		;;
+	# Got lost? Need help? 
+	"-h" | "--help")
 		echo -e "\n${cyan}USAGE: \n${red}myuseradd.sh -a${cyan}    - add a user account \n${red}myuseradd.sh -d${cyan}    - remove a user account \n${red}myuseradd.sh -h${cyan}    - display this usage message\n${def}"
-fi
-# Adding user to the system
-if [[ "$1" = -a ]]
-	then
+		;;
+	# Adding user to the system
+	"-a" | "--add")
 		echo -e "${cyan}Type name of a user to add${red}"
-		read name
-# Making sure the username does not have a twin		
-		user=$(cat /etc/passwd | grep $name | cut -d":" -f1)
+		read -r name
+	# Making sure the username does not have a twin		
+		user=$(grep "$name" /etc/passwd | cut -d":" -f1)
 		if [[ $user = "$name" ]]	
 			then
-				echo -e "${red}Operation forbidden. \nUser $name already exists on the system\nExiting${def}"	
+				echo -e "${red}Operation forbidden. \nUser $name already exists on the system\nExiting${cyan}"	
 				exit
-# Just use 'qwerty'. Nobody cares about the passwords. LOL
 			else
-				echo -e "${cyan}Type password for user $name${def}"
-				read -s password
-				echo -e "${cyan}Confirm the password${def}"
-				read -s password2
-				if [[ $password = $password2 ]]
-# Creating user & user home directory					
-					then
-						mkdir /home/$name
-						useradd $name -d /home/$name -p $password -s /bin/bash
-						chown $name:$name /home/$name
-# Cool effect to visualize running operation						
-						count=50
-						while ((count>0))
-						do
-						echo -e "${cyan}#\c\t100%${def}"
-						sleep 0.02
-						((count--))
-						done
-						echo -e "\n${green}${bold}[User $name was successfully added]${def}"
-# Can't type a few characters correct ? The following message is for you then!					
-					else
-						echo -e "${red}${bold}${blink}[ERROR] ${def}${red}${bold}Passwords for $name did not match\nOperation canceled${def}"
-						exit
-				fi		
+	# Creating user & user home directory					
+				mkdir /home/"$name"
+				useradd -d /home/"$name" -s /bin/bash "$name"
+				passwd "$name"
+				chown "$name":"$name" /home/"$name"
+	# Cool effect to visualize running operation						
+				count=50
+				while ((count>0))
+				do
+				echo -e "${cyan}#\c\t100%${def}"
+				sleep 0.02
+				((count--))
+				done
+				echo -e "\n${green}${bold}｡^‿^｡ [User $name was successfully added]${def}"
 		fi
-fi
-# If user ate your yogurt and not welcome anymore 
-if [[ "$1" = -d ]]
-	then
+		;;
+	# If user ate your yogurt and not welcome anymore 
+	"-d" | "--del" | "--delete")
 		echo -e "${cyan}Type name of a user to delete${red}"
-		read name
-# Making sure someone else did not do this for you in the past		
-		user=$(cat /etc/passwd | grep $name | cut -d":" -f1)
+		read -r name
+	# Making sure someone else did not do this for you in the past		
+		user=$(grep "$name" /etc/passwd | cut -d":" -f1)
 		if [[ $user = "$name" ]]
-# Last chance to apologize			
+	# Last chance to apologize			
 			then
-				echo -e "${cyan}Do you really want to delete user $name ? (y/n)"
-				read answer
+				echo -e "${cyan}◔_◔ Do you really want to delete user $name ? (y/n)"
+				read -r answer
 				if [[ $answer = y ]] || [[ $answer = "" ]]
 					then 
-						userdel -r $name
-# Can you follow '#' each time they appear on the sreen?						
+						userdel -r "$name"
+	# Can you follow '#' each time they appear on the sreen?						
 						count=50
 						while ((count>0))
 						do
@@ -88,11 +76,34 @@ if [[ "$1" = -d ]]
 						sleep 0.02
 						((count--))
 						done
-						echo -e "\n${green}${bold}[User $name was successfully deleted]${def}"
+						echo -e "\n${green}${bold}｡^‿^｡ [User $name was successfully deleted]${def}"
 					else
 						echo -e "${red}${bold}[Operation canceled. User $name was not deleted]${def}"
 				fi
 			else 
-				echo -e "${red}${bold}User $name not found. Exiting${def}"		
+				echo -e "${red}${bold}¯\_(ツ)_/¯  User $name not found. Exiting${def}"		
 		fi
-fi
+		;;
+	
+	"--pikachu")
+	echo -e "\n${yel}
+█▀▀▄           ▄▀▀█
+ █   ▀▄ ▄▄▄▄▄ ▄▀   █
+  ▀▄   ▀     ▀   ▄▀
+    ▌ ▄▄   ▄▄ ▐▀▀
+   ▐  █▄   ▄█  ▌▄▄▀▀▀▀█ 
+   ▌▄▄▀▀ ▄ ▀▀▄▄▐      █
+▄▀▀▐▀▀ ▄▄▄▄▄ ▀▀▌▄▄▄   █
+█   ▀▄ █   █ ▄▀    █▀▀▀
+ ▀▄  ▀  ▀▀▀  ▀   ▄█▀
+   █           ▄▀▄ ▀▄
+   █         ▄▀█  █  █
+   █           █▄█  ▄▀
+   █           ████▀
+   ▀▄▄▀▀▄▄▀▀▄▄▄█\n${def}"	
+	;;
+	*)
+		echo -e "${red}${bold}¯\_(ツ)_/¯  Command not found. Exiting${def}\n${red}For help use ${green}${bold}--help${def}"
+		;;
+
+esac
